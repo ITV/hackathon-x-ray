a2752be7145417394fdc4b6124cceb879f12b70d
tim <tim@gibbodesigns.co.uk>
2022-02-25 08:46:33 +0000

    feat: add animation to show text

--- src/components/show/Show.js
+++ src/components/show/Show.js
@@ -9,15 +9,31 @@ export default function Show() {
         <img alt="Coronation Street" src={showImage} />
       </div>
       <div className="show__content">
-        <h1>Coronation Street</h1>
-        <p>
+        <motion.h1
+          initial={{ x: -100, opacity: 0 }}
+          transition={{ delay: 2.8, duration: 0.7, ease: "easeOut" }}
+          animate={{ x: 0, opacity: 1 }}
+        >
+          Coronation Street
+        </motion.h1>
+        <motion.p
+          initial={{ x: -100, opacity: 0 }}
+          transition={{ delay: 2.9, duration: 0.7, ease: "easeOut" }}
+          animate={{ x: 0, opacity: 1 }}
+        >
           Max's footage of the school dance uncovers shocking events.
           <br />
           Dev blows Chesney's cover in front of a stunned Linda.
           <br />
           Toyah and Imran consider an opportunity to adopt Elsie.
-        </p>
-        <button>Play Episode</button>
+        </motion.p>
+        <motion.button
+          initial={{ x: -100, opacity: 0 }}
+          transition={{ delay: 3, duration: 0.7, ease: "easeOut" }}
+          animate={{ x: 0, opacity: 1 }}
+        >
+          >Play Episode
+        </motion.button>
       </div>
       <div className="show__grad--left"></div>
       <div className="show__grad"></div>
