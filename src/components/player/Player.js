6269b7c26c54c307776c82c7d09ce56d7ec8d138
Raluca Negoita <raluca.negoita@infinityworks.com>
2022-02-25 11:13:39 +0000

    get even function

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,6 +1,19 @@
 import { useRef, useEffect } from "react";
 import ShakaPlayer from "shaka-player-react";
 import "shaka-player/dist/controls.css";
+import timeData from '../../assets/time-data.json';
+
+const lookupTimeEvent = (timeStamp) => {
+  let timeEvent;
+
+  timeData.events.forEach( event => { 
+    if( timeStamp >= event.start && timeStamp < event.end ){
+      timeEvent = event;
+    }
+  });
+
+  return timeEvent;
+}
 
 export default function Player({ show }) {
   const ref = useRef(null);
@@ -13,9 +26,10 @@ export default function Player({ show }) {
     if (ref.current) {
       ref.current.videoElement.addEventListener("pause", () => {
         console.log("I PAUSED THE THING", ref.current.videoElement.currentTime);
+        console.log(lookupTimeEvent(ref.current.videoElement.currentTime));
       });
     }
-  }, []);
+  }, [show]);
 
   return show && <ShakaPlayer ref={ref} src="https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd" />
 }
